92ba0fba465ab49d2298efecdaadf58b16fae339
qz111 <kloseqz@gmail.com>
2022-07-21 18:08:01 +0200

    add test

---
+++ src/main/java/com/example/cicd/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.cicd;
+
+/**
+ * @author qz111
+ * @create 2022-07-7/21/2022 5:42 PM
+ */
+public class Calculator {
+    public static Integer add(Integer a, Integer b){
+        return a+b;
+    }
+}
--- src/test/java/com/example/cicd/CiCdApplicationTests.java
+++ src/test/java/com/example/cicd/CiCdApplicationTests.java
@@ -3,11 +3,21 @@ package com.example.cicd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CiCdApplicationTests {
 
     @Test
     void contextLoads() {
+        try {
+            assertEquals(3, Calculator.add(2, 2));
+            System.out.println("result okay");
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
