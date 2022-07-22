1fa3be0f9e9724969ec14a24ee4c9b7d6a56d0c0
qz111 <kloseqz@gmail.com>
2022-07-22 19:01:22 +0200

    update jenkins deploy process and deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-
-echo "pull image from artifactory"
-sudo docker pull kloseqz/cicdjenkins:v1.0
-echo "docker run as server"
-sudo docker run -p 8801:8080 -d --name mycicdjenkins kloseqz/cicdjenkins:v1.0
